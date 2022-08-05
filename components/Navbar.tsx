b54c9622b2d170b7f7356d1d73cc05b3708906cd
DavidRomeroC <romerocandelariodavid@gmail.com>
2022-08-04 19:22:51 -0500

    pre-deployment vercel

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { ActiveLink } from "./ActiveLink";
+import navbar from "./Navbar.module.css";
+
+const menuItems = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'About',
+        href: '/about'
+    },
+    {
+        text: 'Contact',
+        href: '/contact'
+    },
+    {
+        text: 'Pricing',
+        href: '/pricing'
+    },
+];
+
+export const Navbar = () => {
+    return (
+        <nav className={navbar['menu-container']}>
+            {
+                menuItems.map(({href, text}) => (
+                    <ActiveLink key={text} text={text} href={href} />
+                ))
+            }
+            {/* <ActiveLink text="Home" href="/" />
+            <ActiveLink text="About" href="/about" />
+            <ActiveLink text="Contact" href="/contact" />
+            <ActiveLink text="pricing" href="/pricing" /> */}
+        </nav>
+    )
+}
